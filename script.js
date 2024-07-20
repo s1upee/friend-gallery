926a5df878e2244c90582e22fa0284ecc9426607
s1upee <lisakrasiuk@gmail.com>
2024-07-20 15:53:21 -0400

    Initial commit with basic structure

---
+++ script.js
@@ -0,0 +1,6 @@
+// If you need to add interactivity, such as scrolling effects, you can do it here.
+// Example: Smooth scrolling effect
+
+document.addEventListener("scroll", function() {
+    // Your scroll effect logic here
+});
