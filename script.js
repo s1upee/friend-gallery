0d8f4fb70aac8ac315ff8c8cbae50546fd4c32ef
s1upee <lisakrasiuk@gmail.com>
2024-07-20 16:58:40 -0400

    Added modal with video and Instagram link

--- script.js
+++ script.js
@@ -61,3 +61,22 @@ function toggleMusic() {
         musicControl.textContent = 'Play Music';
     }
 }
+
+// Modal functionality
+const modal = document.getElementById('myModal');
+const span = document.getElementsByClassName('close')[0];
+
+window.onload = function() {
+    modal.style.display = 'block';
+}
+
+span.onclick = function() {
+    modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+
